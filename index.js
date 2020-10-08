ed27d1c52aebde454d5e27f5ae8d39997aa7e253
Robin <mehmet.ucar@imediapro.co.uk>
2020-10-08 16:50:10 +0100

    guess count will work only if user enter a number

--- index.js
+++ index.js
@@ -4,22 +4,25 @@ console.log(randommNumber);
 
 document.querySelector('#check_it').addEventListener('click', checkNumber);
 
-let answer = document.querySelector('#answer');
-
 function checkNumber() {
-  guessCount++;
   let userInput = document.querySelector('#nr_game');
+  let answer = document.querySelector('#answer');
 
-  document.querySelector(
-    '#count_it'
-  ).innerText = `Guess count is: ${guessCount}`;
   if (userInput.value == randommNumber) {
     alert('You are the winner!');
-  } else if (userInput.value == "") {
+  } else if (userInput.value == '') {
     answer.innerText = 'Enter a number!';
-  }else if (userInput.value > randommNumber) {
+  } else if (userInput.value > randommNumber) {
+    guessCount++;
+    document.querySelector(
+      '#count_it'
+    ).innerText = `Guess count is: ${guessCount}`;
     answer.innerText = 'Try a smaller number!';
   } else if (userInput.value < randommNumber) {
+    guessCount++;
+    document.querySelector(
+      '#count_it'
+    ).innerText = `Guess count is: ${guessCount}`;
     answer.innerText = 'Try a bigger number!';
   } else if (isNaN(userInput.value)) {
     answer.innerText = 'Your guess is not a number!';
