1302a7f6b909892fc4fb56c1b4625ef57647b72c
Robin <mehmet.ucar@imediapro.co.uk>
2020-10-08 18:04:18 +0100

    confirm alerts

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 let guessCount = 0;
-const randommNumber = Math.floor(Math.random() * 100 + 1);
+let randommNumber = Math.floor(Math.random() * 100 + 1);
 console.log(randommNumber);
 
 document.querySelector('#check_it').addEventListener('click', checkNumber);
 
-document.querySelector("#nr_game").addEventListener("keyup", function(event) {
+document.querySelector('#nr_game').addEventListener('keyup', function (event) {
   if (event.keyCode === 13) {
     checkNumber();
   }
@@ -13,27 +13,37 @@ document.querySelector("#nr_game").addEventListener("keyup", function(event) {
 function checkNumber() {
   let userInput = document.querySelector('#nr_game');
   let answer = document.querySelector('#answer');
+  let guessElement = document.querySelector('#count_it')
 
   if (userInput.value == randommNumber) {
-    alert('You are the winner!');
+    alert('You are the winner! Guess Time: ' + ++guessCount);
+
+    const choice = confirm('Game again?');
+    if (choice) {
+      guessCount = 0;
+      randommNumber = Math.floor(Math.random() * 100 + 1);
+      answer.innerText = '';
+      guessElement.innerText = '';
+
+    } else {
+      alert('Thanks for gaming!');
+      answer.innerText = 'Best Guess time:' + guessCount;
+      guessElement.innerText = "";
+    }
   } else if (userInput.value == '') {
     answer.innerText = 'Enter a number!';
   } else if (userInput.value > randommNumber) {
     guessCount++;
-    document.querySelector(
-      '#count_it'
-    ).innerText = `Guess count is: ${guessCount}`;
+    guessElement.innerText = `Guess count is: ${guessCount}`;
     answer.innerText = 'Try a smaller number!';
   } else if (userInput.value < randommNumber) {
     guessCount++;
-    document.querySelector(
-      '#count_it'
-    ).innerText = `Guess count is: ${guessCount}`;
+    guessElement.innerText = `Guess count is: ${guessCount}`;
     answer.innerText = 'Try a bigger number!';
   } else if (isNaN(userInput.value)) {
     answer.innerText = 'Your guess is not a number!';
   }
 
   userInput.focus();
-  userInput.value = "";
+  userInput.value = '';
 }
