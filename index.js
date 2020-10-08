28f79dbebbed340775bc8592f6c95165fe1aaf75
Robin <mehmet.ucar@imediapro.co.uk>
2020-10-08 16:03:24 +0100

    More validation options

--- index.js
+++ index.js
@@ -4,14 +4,24 @@ console.log(randommNumber);
 
 document.querySelector('#check_it').addEventListener('click', checkNumber);
 
+let answer = document.querySelector('#answer');
+
 function checkNumber() {
   guessCount++;
   let userInput = document.querySelector('#nr_game');
 
-  document.querySelector("#count_it").innerText = `Guess count is: ${guessCount}`;
+  document.querySelector(
+    '#count_it'
+  ).innerText = `Guess count is: ${guessCount}`;
   if (userInput.value == randommNumber) {
     alert('You are the winner!');
-  } else {
-    alert('You are wrong!');
+  } else if (userInput.value == "") {
+    answer.innerText = 'Enter a number!';
+  }else if (userInput.value > randommNumber) {
+    answer.innerText = 'Try a smaller number!';
+  } else if (userInput.value < randommNumber) {
+    answer.innerText = 'Try a bigger number!';
+  } else if (isNaN(userInput.value)) {
+    answer.innerText = 'Your guess is not a number!';
   }
 }
