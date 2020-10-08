140841da4a377f82afe6a255a4fddc89ef0e174a
Robin <mehmet.ucar@imediapro.co.uk>
2020-10-08 15:36:21 +0100

    guess count

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-
+let guessCount = 0;
 const randommNumber = Math.floor(Math.random() * 100 + 1);
 console.log(randommNumber);
 
-document.querySelector("#check_it").addEventListener('click', checkNumber);
+document.querySelector('#check_it').addEventListener('click', checkNumber);
 
 function checkNumber() {
- let userInput = document.querySelector("#nr_game");
- if (userInput.value == randommNumber) {
-   alert("You are the winner!")
- } else {
-   alert("You are wrong!")
- }
+  guessCount++;
+  let userInput = document.querySelector('#nr_game');
+
+  document.querySelector("#count_it").innerText = `Guess count is: ${guessCount}`;
+  if (userInput.value == randommNumber) {
+    alert('You are the winner!');
+  } else {
+    alert('You are wrong!');
+  }
 }
